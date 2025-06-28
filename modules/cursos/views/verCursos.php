4ebb7b4eb2d3e2691df50fc8ba01ff2a36c6c0f7
Daniel <2022160045@udh.edu.pe>
2025-06-28 12:05:29 -0500

    mostra en una tabla los cursos que se tienen

--- modules/cursos/views/verCursos.php
+++ modules/cursos/views/verCursos.php
@@ -1,6 +1,33 @@
 <?php
-require_once '/controllers/CursosController.php';
-$curso = new CursosController();
-$cursos = $curso->mostrar();
+require_once '../controllers/CursosControllers.php';
+$curs = new CursosController();
+$cursos = $curs->mostrar();
 ?>
 <h1 class="mt-5">Cursos de la Academia E</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>Nomble del curso</th>
+            <th>Duracion</th>
+            <th>Estado</th>
+            <th>Costo</th>
+            <th>Id del aula</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach($cursos as $curso){
+        echo "<tr>
+                <td>".$curso["id_curso"]."</td>
+                <td>".$curso["nombre_curso"]."</td>
+                <td>".$curso["duracion"]."</td>
+                <td>".$curso["estado"]."</td>
+                <td>".$curso["costo"]."</td>
+                <td>".$curso["id_aula"]."</td>
+            </tr>";
+
+    }
+    ?>
+    </tbody>
+</table>
